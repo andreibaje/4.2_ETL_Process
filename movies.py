76ba37e95593fc91602c105333b921a24e8bc84f
andreibaje <andreibajenaru2012@gmail.com>
2025-04-17 14:35:05 +0200

    Made an initial script for the etl_process

--- movies.py
+++ movies.py
@@ -6,9 +6,12 @@ output_folder = "result"
 os.makedirs(output_folder, exist_ok=True)
 df = pd.read_csv("movies.csv")
 df.columns = df.columns.str.strip().str.lower()
+df["budget"] = pd.to_numeric(df["budget"].replace('[\$,]', '', regex=True), errors='coerce')
+df["box_office"] = pd.to_numeric(df["box_office"].replace('[\$,]', '', regex=True), errors='coerce')
 df["bilanț"] = df["box_office"] - df["budget"]
 for country in countries:
     df_country = df[df["country"] == country]
+    df_country = df_country.drop(columns=["language", "country", "duration", "budget", "box_office"], errors="ignore")
     df_filtered = df_country[["title", "release_year", "genre", "director", "bilanț"]].copy()
     df_filtered = df_filtered.sort_values(by="bilanț", ascending=False)
     top_10 = df_filtered.head(10)
