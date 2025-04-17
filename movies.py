3500fe8f44ae8d293900de9dc8690d15aca9213f
andreibaje <andreibajenaru2012@gmail.com>
2025-04-17 14:26:30 +0200

    Addition of the movie list and a etl_movies file

---
+++ movies.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+countries = ["USA", "Russia", "UK", "South Korea"]
+output_folder = "result"
+os.makedirs(output_folder, exist_ok=True)
+df = pd.read_csv("movies.csv")
+df.columns = df.columns.str.strip().str.lower()
+df["bilanț"] = df["box_office"] - df["budget"]
+for country in countries:
+    df_country = df[df["country"] == country]
+    df_filtered = df_country[["title", "release_year", "genre", "director", "bilanț"]].copy()
+    df_filtered = df_filtered.sort_values(by="bilanț", ascending=False)
+    top_10 = df_filtered.head(10)
+    name_file = f"{country.replace(' ', '_')}_top_10.xlsx"
+    path_file = os.path.join(output_folder, name_file)
+    top_10.to_excel(path_file, index=False)
+
+print("The files have been made in the folder 'output'.")
